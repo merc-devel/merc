6287318e03d76d7b84fb2629a0b15b2c8f13e3d7
Tony Young <tony@rfw.name>
2014-10-20 21:14:10 +1100

    implement message truncation

--- merc/messages/message.py
+++ merc/messages/message.py
@@ -2,8 +2,11 @@ from merc import emitter
 
 
 class Message(object):
+  MAX_LENGTH = 510
+
   def as_params(self, client):
     raise NotImplementedError
 
   def emit(self, client, prefix):
-    return emitter.emit_message(prefix, self.NAME, self.as_params(client))
+    raw = emitter.emit_message(prefix, self.NAME, self.as_params(client))
+    return raw.encode("utf-8")[:self.MAX_LENGTH].decode("utf-8", "ignore")
