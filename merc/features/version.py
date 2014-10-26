2acde899f0e44c860a329d5eb9f85f51747d5339
Tony Young <tony@rfw.name>
2014-10-26 11:25:57 +1100

    fix indent

--- merc/features/version.py
+++ merc/features/version.py
@@ -35,10 +35,9 @@ class Version(message.Command):
   @message.Command.requires_registration
   def handle_for(self, user, prefix):
     if self.server and self.server != user.server.name:
-        raise errors.NoSuchServer(self.server)
-    else:
-        server = user.server
+      raise errors.NoSuchServer(self.server)
 
+    server = user.server
     version = 'merc-{}'.format(server.version)
     user.send_reply(VersionReply(version, server.name, "..."))
     user.send_reply(welcome.ISupport(server.isupport))
