767145270b1520cb0d7a50ee10f508f32dee3bff
Tony Young <tony@rfw.name>
2014-10-27 23:34:48 +1100

    add argument parser

--- merc/__main__.py
+++ merc/__main__.py
@@ -1,3 +1,4 @@
+import argparse
 import coloredlogs
 import logging
 import yaml
@@ -7,4 +8,11 @@ from merc import server
 coloredlogs.install(level=logging.INFO)
 logging.getLogger("asyncio").setLevel(logging.WARN)
 
-server.Server("merc.conf").start()
+parser = argparse.ArgumentParser(
+    formatter_class=argparse.ArgumentDefaultsHelpFormatter)
+parser.add_argument("--config", "-c", help="file to load configuration from",
+                    default="merc.conf")
+
+args = parser.parse_args()
+
+server.Server(args.config).start()
