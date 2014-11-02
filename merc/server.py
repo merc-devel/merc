b89e049c55a504e299134cde3bdffd11bed8b7a0
Tony Young <tony@rfw.name>
2014-11-02 22:57:52 +1100

    remove multicast_to_neighbors

--- merc/server.py
+++ merc/server.py
@@ -224,10 +224,6 @@ class Network(object):
     for name in self.tree.neighbors(self.current.name):
       yield self.get(name)
 
-  def multicast_to_neighbors(self, prefix, message):
-    for neighbor in self.neighborhood():
-      neighbor.send(prefix, message)
-
   def broadcast(self, user, prefix, message):
     for channel in user.channels.values():
       channel.broadcast(user, prefix, message)
